fd50e2e9a06fe13033912056a4e33f3c2b902f64
calexandar <alexandercvet243@gmail.com>
2026-02-09 10:28:34 +0100

    feat: generate image

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Models\User;
-use Laravel\Ai\Files\Image;
+use Laravel\Ai\Image;
 use function Laravel\Ai\agent;
-use function Laravel\Prompts\text;
 
+use function Laravel\Prompts\text;
 use App\Ai\Agents\PersonalAssistant;
 use Illuminate\Support\Facades\Artisan;
 use App\Ai\Tools\GiveOneOfMySubscribers;
@@ -35,4 +35,11 @@ Artisan::command('test:tool', function() {
         new GiveOneOfMySubscribers(),
     ]
    )->prompt('Give me one of my subscribers.');
+});
+
+Artisan::command('generate:image', function() {
+    $image = Image::of('A donut sitting on the kitchen counter')->generate();
+ 
+    $image->storeAs('image.jpg');
+    $this->info('Image generated and stored as image.jpg');
 });
